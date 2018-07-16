84a4f416990a305942a371795a5bbcc57fd4b949
ScottAP108 <saparton@gmail.com>
2018-07-16 09:36:41 -0700

    removed asidaco-solar link, made site responsive

--- client/js/views/templates/Home.js
+++ client/js/views/templates/Home.js
@@ -6,7 +6,7 @@ module.exports = function() {
         <p>Asidaco, LLC provides honest and dependable residential, and commercial electrical work.</p>
         <p>We specialize in Engineering, Procurement & Construction (EPC) providing electrical repair services, renewable energy solutions, power quality and distribution services, and electrical system design and installation services and products. We not only offer all our services as one-stop, turnkey solutions, but we are also among the region’s top consultants. Our engineers, project managers, and experienced electricians are ready to serve you.</p>
         <p>Our client services are: engineered drawings and pre-construction management, electrical services ranging from repair to construction, back-up power, power quality, utility integration consulting and construction, solar power installations, energy storage and power conditioning solutions, electric vehicle charging stations, solar and wind energy site assessment, site development, management and construction.</p>
-        <p>For more information about our solar solutions go to <a href="http://www.asidaco-solar.com">asidaco-solar.com</a> or click <span class="link" data-js="link" data-link="solar">HERE</span></p>
+        <p>For more information about our solar solutions click <span class="link" data-js="link" data-link="solar">HERE</span></p>
         <p>We are a full service licensed electrical contractor serving Dayton and surrounding areas.</p>
         <p>Renewable energy, power engineering, design, project development, & construction - that's what we do.</p>
         <h2>Licensed - Bonded & Insured</h2>
